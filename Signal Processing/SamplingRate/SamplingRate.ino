059ef48137e473e838c07e33db01c70af217a05b
Rengganis R. H. Santoso <rengganis.ridwan@gmail.com>
2024-06-10 00:10:09 +0700

    Update SamplingRate.ino

--- Signal Processing/SamplingRate/SamplingRate.ino
+++ Signal Processing/SamplingRate/SamplingRate.ino
@@ -4,11 +4,14 @@
 
   Author: Rengganis R.H. Santoso
 */
+#include <MegunoLink.h>
 
-const unsigned long kSamplingRateHz = 500;
-const unsigned long kBaudRate = 115200;
-const double kSignalFreqHz = 3;
+const unsigned long kSamplingRateHz = 300;
+const unsigned long kBaudRate = 500000;
+const double kSignalFreqHz = 1;
 const double kNoiseFreqHz = 50;
+
+TimePlot MyPlot;
 
 void setup() {
   Serial.begin(kBaudRate);
@@ -16,24 +19,26 @@ void setup() {
 
 void loop() {
   // Calculate elapsed time
-  static unsigned long past_time_us = 0;                            // Initiate static variable to store previous time
-  unsigned long present_time_us = micros();                         // Obtain current time
-  unsigned long time_interval_us = present_time_us - past_time_us;  // Calculate the elapsed time
-  past_time_us = present_time_us;                                   // Store the current time as the previous time for the next loop
+  static unsigned long t_past_us = 0;
+  unsigned long t_present_us = micros();
+  unsigned long t_interval_us = t_present_us - t_past_us;
+  t_past_us = t_present_us;
 
-  // Subtract the timer by the elapsed time between subsequent loop. If the
-  // timer value reaches zero, we do the measurement. Then, the timer value will
-  // be added by the sampling time period. Thus, the measurement will be
+  // Subtract the timer by the elapsed time between subsequent loop. If the 
+  // timer value reaches zero, we do the measurement. Then, the timer value 
+  // will be added by the sampling time period. Thus, the measurement will be 
   // conducted in a periodic manner.
   static long timer_us = 0;
-  timer_us -= time_interval_us;
+  timer_us -= t_interval_us;
   if (timer_us < 0) {
-    timer_us += 1000000 / kSamplingRateHz;
+    timer_us += 1e6 / kSamplingRateHz;
     double t = micros() / 1.0e6;
 
-    double input_sig = sin(2 * PI * kSignalFreqHz * t);
-    double noise_sig = 0.5 * sin(2 * PI * kNoiseFreqHz * t);
-    double combined_sig = input_sig + noise_sig;
-    Serial.println(combined_sig);
+    double input_signal = sin(2 * PI * kSignalFreqHz * t);
+    double noise_signal = 0.5 * sin(2 * PI * kNoiseFreqHz * t);
+    double combined_signal = input_signal + noise_signal;
+    
+    // MyPlot.SendData("t",millis());
+    MyPlot.SendData("CH-01",combined_signal);
   }
 }
